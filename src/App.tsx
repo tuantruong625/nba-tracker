e0b07d1de1cf40ee31076bfa028ea38c555f8410
Tuan N. Truong <tuan.truong625@gmail.com>
2021-11-26 17:35:36 -0500

    Add logos

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import { union } from 'lodash'
+import { teamLogos } from './data';
+
 
 function App() {
+
+  type ITeam = {
+    id: number;
+    abbreviation: string;
+    city: string;
+    conference: string;
+    division: string;
+    full_name: string;
+    name: string;
+    img: string;
+  }
+  const [teams, setTeams] = useState<ITeam[]>([])
+
+  const getTeams = async () => {
+    const { data } = await axios.get('https://www.balldontlie.io/api/v1/teams')
+    setTeams(data.data)
+  }
+
+  const displayLogo = (teamName = 'Raptors'): string | undefined => {
+    const logo = teamLogos.find(teamLogo => {
+      return teamLogo.name === teamName
+    })
+
+    return logo?.src
+  }
+
+  useEffect(() => {
+    getTeams()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Nba Tracker</h1>
+      {
+        teams && teams.map((team) => (
+          <>
+            <div>{team.name}</div>
+            <img src={displayLogo(team.name)} alt={`${team.name} logo`} style={{ width: '50px' }} />
+          </>
+        ))
+      }
     </div>
   );
 }
