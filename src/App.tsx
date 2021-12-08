5258587c8128edd2811d70198f666576c1544473
Tuan N. Truong <tuan.truong625@gmail.com>
2021-12-08 17:35:52 -0500

    Update image urls

--- src/App.tsx
+++ src/App.tsx
@@ -64,7 +64,9 @@ function App() {
               <p className="text-gray-600">{todaysDate.toFormat('EEEE, MMM dd, yyyy')} </p>
               <div>
                 <div className="flex">
-                  <h1 className="text-3xl font-bold mr-4">Today's Games</h1>
+                  <button onClick={() => { setTodaysDate(todaysDate) }}>
+                    <h1 className="text-3xl font-bold mr-4">Today's Games</h1>
+                  </button>
                   <button className="bg-gray-50 p-1 w-10 h-10 rounded-full text-gray-500 hover:bg-blue-500 hover:text-gray-50 hover:shadow-lg" onClick={() => { setTodaysDate(todaysDate.plus({ days: -1 })) }}>←</button>
                   <button className="bg-gray-50 p-1 w-10 h-10 rounded-full text-gray-500 hover:bg-blue-500 hover:text-gray-50 hover:shadow-lg" onClick={() => { setTodaysDate(todaysDate.plus({ days: 1 })) }}>→</button>
                 </div>
@@ -76,8 +78,8 @@ function App() {
                 <div className="col-span-6" key={id}>
                   <div className="flex justify-around items-center bg-gray-50 border rounded-sm p-2">
                     <div className="flex flex-col items-center flex-1">
-                      <img src={displayLogo(home_team.name)} alt={`${home_team.name} logo`} style={{ width: '50px' }} />
-                      <p>{home_team.name}</p>
+                      <img src={displayLogo(home_team.name)} alt={`${home_team.name} logo`} className="w-16" />
+                      <p className="text-sm pt-1 text-gray-700">{home_team.name}</p>
                     </div>
                     <div className="flex items-center justify-center flex-1">
                       <p className={`text-2xl ${home_team_score > visitor_team_score ? 'text-green-500' : 'text-gray-800'}`}>{home_team_score === 0 ? '-' : home_team_score}</p>
@@ -85,8 +87,8 @@ function App() {
                       <p className={`text-2xl ${visitor_team_score > home_team_score ? 'text-green-500' : 'text-gray-800'}`}>{visitor_team_score === 0 ? '-' : visitor_team_score}</p>
                     </div>
                     <div className="flex flex-col items-center p-4 flex-1">
-                      <img src={displayLogo(visitor_team.name)} alt={`${visitor_team.name} logo`} style={{ width: '50px' }} />
-                      <p>{visitor_team.name}</p>
+                      <img src={displayLogo(visitor_team.name)} alt={`${visitor_team.name} logo`} className="w-16" />
+                      <p className="text-sm pt-1 text-gray-700">{visitor_team.name}</p>
                     </div>
                   </div>
                 </div>
