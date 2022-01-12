5521d0b7118b41abd3b7a57d807e5584324f9691
Tuan N. Truong <tuan.truong625@gmail.com>
2022-01-12 13:15:12 -0500

    Make layout responsive

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -13,18 +13,18 @@ const Games = ({ todaysGames, onSetSelectedGame }: GameProps): JSX.Element => {
  return <>
   {
    todaysGames.map(game => (
-    <div className="col-span-6" key={game.id}>
-     <Link to={`/game-stats/${game.id}`} className="flex justify-around items-center bg-gray-50 border rounded-sm p-2" onClick={() => onSetSelectedGame(game.id.toString())}>
+    <div className="col-span-12 md:col-span-6" key={game.id}>
+     <Link to={`/game-stats/${game.id}`} className="flex justify-around items-center bg-gray-50 border rounded-sm p-4" onClick={() => onSetSelectedGame(game.id.toString())}>
       <div className="flex flex-col items-center flex-1">
        <img src={displayLogo(game.home_team.name)} alt={`${game.home_team.name} logo`} className="w-16" />
        <p className="text-sm pt-1 text-gray-700">{game.home_team.name}</p>
       </div>
       <div className="flex items-center justify-center flex-1">
-       <p className={`text-2xl ${game.home_team_score > game.visitor_team_score ? 'text-green-500' : 'text-gray-800'}`}>{game.home_team_score === 0 ? '-' : game.home_team_score}</p>
-       <p className="text-gray-700 px-6">{game.status}</p>
-       <p className={`text-2xl ${game.visitor_team_score > game.home_team_score ? 'text-green-500' : 'text-gray-800'}`}>{game.visitor_team_score === 0 ? '-' : game.visitor_team_score}</p>
+       <p className={`text-2xl ${game.home_team_score > game.visitor_team_score ? 'text-green-500' : 'text-gray-800'}`}>{game.home_team_score === 0 ? '' : game.home_team_score}</p>
+       <p className="text-gray-700 px-2 md:px-6 text-sm md:text-base">{game.status}</p>
+       <p className={`text-2xl ${game.visitor_team_score > game.home_team_score ? 'text-green-500' : 'text-gray-800'}`}>{game.visitor_team_score === 0 ? '' : game.visitor_team_score}</p>
       </div>
-      <div className="flex flex-col items-center p-4 flex-1">
+      <div className="flex flex-col items-center flex-1">
        <img src={displayLogo(game.visitor_team.name)} alt={`${game.visitor_team.name} logo`} className="w-16" />
        <p className="text-sm pt-1 text-gray-700">{game.visitor_team.name}</p>
       </div>
--- src/pages/gameStats/index.tsx
+++ src/pages/gameStats/index.tsx
@@ -7,72 +7,72 @@ import { GameStatsType, GameType } from '../../types'
 import { displayLogo } from '../../utils'
 
 const GameStats = ({ todaysGames }: { todaysGames: GameType[] }) => {
- let params: any = useParams()
- const [gameStats, setGameStats] = useState<GameStatsType[]>([])
- const [selectedGame] = useState(params.gameId)
- const [game, setGame] = useState<GameType>()
+  let params: any = useParams()
+  const [gameStats, setGameStats] = useState<GameStatsType[]>([])
+  const [selectedGame] = useState(params.gameId)
+  const [game, setGame] = useState<GameType>()
 
- useEffect(() => {
-  (async () => {
-   try {
-    const { data } = await axios.get(`https://www.balldontlie.io/api/v1/stats?game_ids[]=${selectedGame}&per_page=100`, {
-     headers: { "Access-Control-Allow-Origin": "*" }
-    })
-    setGameStats(data.data)
-   } catch (e) {
-    console.error(e)
-   }
-  })()
-  setGame(todaysGames.find(game => game.id === parseInt(selectedGame)))
- }, [todaysGames, game, selectedGame])
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await axios.get(`https://www.balldontlie.io/api/v1/stats?game_ids[]=${selectedGame}&per_page=100`, {
+          headers: { "Access-Control-Allow-Origin": "*" }
+        })
+        setGameStats(data.data)
+      } catch (e) {
+        console.error(e)
+      }
+    })()
+    setGame(todaysGames.find(game => game.id === parseInt(selectedGame)))
+  }, [todaysGames, game, selectedGame])
 
- return (
-  <>
-   <Link to="/">Back</Link>
-   <div className="col-span-full">
-    <div className="flex justify-around items-center bg-gray-50 border rounded-sm p-2">
-     <div className="flex flex-col items-center flex-1">
-      <img src={displayLogo(game?.home_team?.name)} alt={`${game?.home_team.name} logo`} className="w-16" />
-      <p className="text-sm pt-1 text-gray-700">{game?.home_team.name}</p>
-     </div>
-     <div className="flex items-center justify-center flex-1">
-      <p className={`text-2xl`}>{game?.home_team_score === 0 ? '-' : game?.home_team_score}</p>
-      <p className="text-gray-700 px-6">{game?.status}</p>
-      <p className={`text-2xl `}>{game?.visitor_team_score === 0 ? '-' : game?.visitor_team_score}</p>
-     </div>
-     <div className="flex flex-col items-center p-4 flex-1">
-      <img src={displayLogo(game?.visitor_team.name)} alt={`${game?.visitor_team.name} logo`} className="w-16" />
-      <p className="text-sm pt-1 text-gray-700">{game?.visitor_team.name}</p>
-     </div>
-    </div>
-    {
-     gameStats.length ? <>
-      <p className="pt-3 text-lg text-gray-800">
-       Top Performers
-      </p>
+  return (
+    <>
+      <Link to="/">Back</Link>
+      <div className="col-span-full">
+        <div className="flex justify-around items-center bg-gray-50 border rounded-sm p-4">
+          <div className="flex flex-col items-center flex-1">
+            <img src={displayLogo(game?.home_team?.name)} alt={`${game?.home_team.name} logo`} className="w-16" />
+            <p className="text-sm pt-1 text-gray-700">{game?.home_team.name}</p>
+          </div>
+          <div className="flex items-center justify-center flex-1">
+            <p className={`text-2xl`}>{game?.home_team_score === 0 ? '' : game?.home_team_score}</p>
+            <p className="text-gray-700 px-2 md:px-6">{game?.status}</p>
+            <p className={`text-2xl `}>{game?.visitor_team_score === 0 ? '' : game?.visitor_team_score}</p>
+          </div>
+          <div className="flex flex-col items-center flex-1">
+            <img src={displayLogo(game?.visitor_team.name)} alt={`${game?.visitor_team.name} logo`} className="w-16" />
+            <p className="text-sm pt-1 text-gray-700">{game?.visitor_team.name}</p>
+          </div>
+        </div>
+        {
+          gameStats.length ? <>
+            <p className="pt-3 text-lg text-gray-800">
+              Top Performers
+            </p>
 
-      <div className="col-span-full grid grid-cols-6 gap-4 pt-3">
-       <TopPerformers gameStats={gameStats} teamName={game?.home_team.name} />
-       <TopPerformers gameStats={gameStats} teamName={game?.visitor_team.name} />
+            <div className="col-span-full grid sm:grid-col-12 md:grid-cols-6 gap-4 pt-3">
+              <TopPerformers gameStats={gameStats} teamName={game?.home_team.name} />
+              <TopPerformers gameStats={gameStats} teamName={game?.visitor_team.name} />
+            </div>
+
+            <div className="col-span-full grid sm:grid-col-12 md:grid-cols-6 gap-4 pt-3">
+              <div className='overflow-auto col-span-3 border rounded'>
+                <StatsTable teamName={game?.home_team.name} gameStats={gameStats} />
+              </div>
+
+              <div className='overflow-auto col-span-3 border rounded'>
+                <StatsTable teamName={game?.visitor_team.name} gameStats={gameStats} />
+              </div>
+            </div>
+          </> :
+            <div className='flex justify-center items-center pt-12'>
+              <h2 className='text-2xl text-gray-700'>🙅 Game has not started yet 🙅</h2>
+            </div>
+        }
       </div>
-
-      <div className="col-span-full grid grid-cols-6 gap-4 pt-3">
-       <div className='overflow-auto col-span-3 border rounded'>
-        <StatsTable teamName={game?.home_team.name} gameStats={gameStats} />
-       </div>
-
-       <div className='overflow-auto col-span-3 border rounded'>
-        <StatsTable teamName={game?.visitor_team.name} gameStats={gameStats} />
-       </div>
-      </div>
-     </> :
-      <div className='flex justify-center items-center pt-12'>
-       <h2 className='text-2xl text-gray-700'>🙅 Game has not started yet 🙅</h2>
-      </div>
-    }
-   </div>
-  </>
- )
+    </>
+  )
 }
 
 export default GameStats
